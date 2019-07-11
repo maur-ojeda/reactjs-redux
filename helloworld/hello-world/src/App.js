9829e7218d875ac9cd8bf61b2f1ca4d8f798b89d
Mauricio Ojeda <m.ojeda.b@icloud.com>
2019-07-11 17:10:38 -0400

    hello world

---
+++ helloworld/hello-world/src/App.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Lorem sahfduñjbfhuiiwjhfu owshf uwehfs uoiwef
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+      se cambia 
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+
+
+
+
+//export default wawa;
+
+
+export default App;
