2069d6fde4a25089811f96da0144a7d3a3df7468
Mauricio Ojeda <m.ojeda.b@icloud.com>
2019-07-12 16:32:09 -0400

    Creacion de h1

--- helloworld/hello-world/src/components/Cabecera.jsx
+++ helloworld/hello-world/src/components/Cabecera.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import logo from '../logo.svg';
-
+import H1 from './H1';
 
 
 const styles = {
@@ -43,7 +43,7 @@ export default class Cabecera  extends Component {
         return(
             <header onClick={this.cambiaColorHeader} style = {styles.header({ backgroundColor })} >
                 <img onClick={this.manejaClick} src={logo} className="App-logo" alt="logo"/>
-                    <p>{miau}</p>
+                    <H1>{miau}</H1>
                     <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">se cambia</a>
             </header>
         )
---
+++ helloworld/hello-world/src/components/H1.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+
+
+const style = { 
+    fontSize: '1.5em'
+}
+
+
+
+export default class H1 extends Component {
+    render(){
+        return(
+            <h1 {...this.props} style={style} />
+        )
+    }
+}
