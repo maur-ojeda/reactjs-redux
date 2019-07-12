7c892067b91a51c18adfb1dede4114a37334c229
Mauricio Ojeda <m.ojeda.b@icloud.com>
2019-07-12 10:44:11 -0400

    props -la funcion no esta bien

--- helloworld/hello-world/src/App.js
+++ helloworld/hello-world/src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 
 
 class App extends Component {
+  manejaClick = texto => { 
+    console.log(texto) 
+  }
   render() {
+    const miau = 'texto de miau'
+    const guau = 'texto de guau'
     return (
       <div className="App">
-        <Cabecera/>
+         <header className="App-header" >
+                <Cabecera miau={miau} manejaClick={this.manejaClick}/>
+                <Cabecera miau={guau} manejaClick={this.manejaClick}/>
+        </header>
       </div>
     );
   }
 }
-
-
-
-
-
-//export default wawa;
-
-
 export default App;
--- helloworld/hello-world/src/components/Cabecera.jsx
+++ helloworld/hello-world/src/components/Cabecera.jsx
@@ -3,22 +3,23 @@ import logo from '../logo.svg';
 
 
 export default class Cabecera  extends Component {
+    manejaClick = () => {
+        const { miau, manejaClick } = this.props
+        manejaClick(miau)
+
+    }
+
     render(){
+
+        const { miau, manejaClick } = this.props
+
         return(
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                    cabecera
-                    </p>
-                    <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    >
-                    se cambia 
-                    </a>
-            </header>
+            <div>
+                <img onClick={this.manejaClick} src={logo} className="App-logo" alt="logo"/>
+                    <p>{miau}</p>
+                    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">se cambia</a>
+
+                </div>
         )
     }
 }
