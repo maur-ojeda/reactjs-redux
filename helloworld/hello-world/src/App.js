105a66c53a08b4447066b18b21e241d5a46f6721
Mauricio Ojeda <m.ojeda.b@icloud.com>
2019-07-22 18:16:20 -0400

    ajax get y post

--- helloworld/hello-world/src/App.js
+++ helloworld/hello-world/src/App.js
@@ -1,6 +1,30 @@
 import React, {Component} from 'react';
 import FormularioSimple from './components/FormularioSimple'
 class App extends Component {
+
+constructor() {
+  //llama al constructor de la clase que se esta extendiendo en este caso componenet
+  super()
+  // get
+  //fetch('https://jsonplaceholder.typicode.com/users')
+  //  .then(x => x.json())
+  //  .then(x => console.log(x))
+
+  //post
+  fetch('https://jsonplaceholder.typicode.com/users',{
+    method: 'POST',
+    headers: {
+      'Content-Type' : 'application/json'
+    },
+      body: JSON.stringify({
+        name: 'mauricio Ojeda',
+        username: 'mojeda',
+      })
+  })
+  .then(x=> x.json())
+  .then(x=> console.log(x))
+}
+
   render() {
     return (
       <div className="App">
