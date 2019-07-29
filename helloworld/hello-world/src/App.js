1e0b408a6aec2c0a5f8152631782715f5abf3e5f
Mauricio Ojeda <m.ojeda.b@icloud.com>
2019-07-29 12:43:27 -0400

    instalación de axios . prueba de  get y post

--- helloworld/hello-world/src/App.js
+++ helloworld/hello-world/src/App.js
@@ -1,11 +1,25 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import FormularioSimple from './components/FormularioSimple'
+
 
 
 
 class App extends Component {
 constructor() {
   super()
+
+ // axios.get('https://jsonplaceholder.typicode.com/users')
+ // .then(({data}) => console.log(data))
+
+ axios.post('https://jsonplaceholder.typicode.com/users',
+ {
+  name: 'Mauricio Ojeda' ,
+  username : 'moeb'
+ })
+ .then(({data}) => console.log(data))
+
+
 }
 
   render() {
